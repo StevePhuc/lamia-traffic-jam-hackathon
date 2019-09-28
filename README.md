611559ebb34a8527afc8c11b4ecb5c45ecebcd60
Steve Phuc <stevephuc.dev@gmail.com>
2019-09-28 15:30:23 +0300

    setup with index.html

---
+++ README.md
@@ -0,0 +1,3 @@
+## Tram Jam project - Lamia Flow HSL Hackathon
+
+Tram Jam project, in which the broad idea is to estimate tram congestion (ie. how full of people a tram is) based on tram stop times and other real-time data.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./App.css";
+// import MyMap from "./MyMap";
+// import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+
+function App() {
+    const position = [51.505, -0.09];
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+        </div>
+    );
+}
+
+export default App;
