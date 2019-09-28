3686082785935dcbcfc57ac63c69235d009d84cb
Steve Phuc <stevephuc.dev@gmail.com>
2019-09-29 00:21:20 +0300

    all trams

--- src/MyMap.js
+++ src/MyMap.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 import L from "leaflet";
-// import TramPos from "./TramPos";
 let trams = [];
 let mqtt = require("mqtt");
 let client = mqtt.connect("wss://mqtt.hsl.fi:443/");
@@ -11,10 +10,7 @@ let client = mqtt.connect("wss://mqtt.hsl.fi:443/");
 client.on("connect", function() {
     console.log("client is connected");
 });
-client.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/1007/+/#", { qos: 1 }, function(
-    error,
-    granted
-) {
+client.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/#", { qos: 1 }, function(error, granted) {
     if (error) {
         console.log(error);
     } else {
@@ -25,21 +21,19 @@ client.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/1007/+/#", { qos: 1 }, fun
 client.on("message", function(topic, message) {
     const data = JSON.parse(message);
     // console.log(data.VP);
-    const { veh, lat, long } = data.VP;
+    const { desi, veh, lat, long } = data.VP;
     // console.log("veh:", veh, "lat:", lat, "long:", long);
     const existTram = trams.find(tram => {
         return tram.veh === veh;
     });
     //
     if (!existTram) {
-        // console.log("add tram", veh);
-        trams = [...trams, { veh, lat, long }];
+        trams = [...trams, { desi, veh, lat, long }];
     } else {
         if (!lat || !long) {
             return;
         }
-        trams = trams.map(tram => (tram.veh == veh ? { veh, lat, long } : tram));
-        // console.log("trams", trams[4]);
+        trams = trams.map(tram => (tram.veh == veh ? { desi, veh, lat, long } : tram));
     }
     //
 });
@@ -55,25 +49,12 @@ export default () => {
     const position = [map.lat, map.lng];
 
     const [tramsArray, setTramsArray] = useState([]);
-    // const [trams, setTrams] = useState([]);
 
-    // const [tramPos, setTramPos] = useState([60.152442, 24.918126]);
     const [tramColor, setTramColor] = useState("icon-green");
-
-    const iconTram = new L.Icon({
-        iconUrl: require("./img/number7.svg"),
-        iconAnchor: null,
-        popupAnchor: null,
-        shadowUrl: null,
-        shadowSize: null,
-        shadowAnchor: null,
-        iconSize: new L.Point(30, 30),
-        className: tramColor
-    });
 
     useEffect(() => {
         setInterval(() => {
-            console.log(trams);
+            // console.log(trams);
             setTramsArray(trams);
         }, 1000);
     }, []);
@@ -100,14 +81,29 @@ export default () => {
     return (
         <>
             {/* <TramPos checkTram={checkTram} /> */}
-            <Map style={{ height: "70vh" }} center={position} zoom={map.zoom} zoomControl={false}>
+            <Map
+                style={{ height: "80vh" }}
+                center={position}
+                zoom={map.zoom}
+                zoomControl={false}
+                scrollWheelZoom={false}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {tramsArray.map(tram => {
                     const tramPosition = [tram.lat, tram.long];
-                    return <Marker key={tram.veh} position={tramPosition} icon={iconTram}></Marker>;
+                    return (
+                        <Marker
+                            key={tram.veh}
+                            position={tramPosition}
+                            icon={L.divIcon({
+                                className: `my-div-icon vh-${tram.veh} route desi-${tram.desi} `
+                            })}
+                            opacity={0.8}
+                        ></Marker>
+                    );
                 })}
             </Map>
         </>
