c9cdc23a7d80ca8ce805f1799e2a72d0a06aabb6
Steve Phuc <stevephuc.dev@gmail.com>
2019-09-28 17:16:47 +0300

    check congestionRate add color

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,18 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 import L from "leaflet";
-
-const iconPerson = new L.Icon({
-    iconUrl: require("./img/number7.svg"),
-    iconAnchor: null,
-    popupAnchor: null,
-    shadowUrl: null,
-    shadowSize: null,
-    shadowAnchor: null,
-    iconSize: new L.Point(40, 40),
-    className: "icon-green"
-});
 
 export default () => {
     const map = {
@@ -24,7 +13,39 @@ export default () => {
 
     const position = [map.lat, map.lng];
 
-    const [tram, setTram] = useState([60.152442, 24.918126]);
+    const [tramPos, setTramPos] = useState([60.152442, 24.918126]);
+    const [tramColor, setTramColor] = useState("icon-green");
+
+    const iconTram = new L.Icon({
+        iconUrl: require("./img/number7.svg"),
+        iconAnchor: null,
+        popupAnchor: null,
+        shadowUrl: null,
+        shadowSize: null,
+        shadowAnchor: null,
+        iconSize: new L.Point(40, 40),
+        className: tramColor
+    });
+
+    useEffect(() => {
+        const url =
+            "https://hsl-tram-congestion.appspot.com/trips/HSL:1007_20190820_Ti_2_2101/congestionRate";
+        const fetchData = async () => {
+            const result = await fetch(url);
+            const data = await result.text();
+            const congestionRate = parseFloat(data);
+            console.log(congestionRate);
+
+            if (congestionRate > 1) {
+                setTramColor("icon-red");
+            } else if (congestionRate > 0.5) {
+                setTramColor("icon-yellow");
+            } else {
+                setTramColor("icon-green");
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <Map style={{ height: "70vh" }} center={position} zoom={map.zoom}>
@@ -32,7 +53,7 @@ export default () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={tram} icon={iconPerson}>
+            <Marker position={tramPos} icon={iconTram}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
